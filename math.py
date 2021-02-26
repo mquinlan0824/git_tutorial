42a28dd4f6705062d64b437960b785e1ca7e7ab1
unknown <[mquinlan0824@gmail.com]>
2021-02-26 21:27:11 +0100

    updated functions

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
 # I need to implement this
 def add(x,y):
-    z = x + y
-    return z
+    if (type(x)==int and type(y)==int):
+        return x + y
+    return None
 
 # Bob needs to implement this
 def mult(x,y):
-    return x*y
+    print(type(x))
+    if (type(x)==int and type(y)==int):
+        return x * y
+    return None
 
-z = add(4,5)
+z = mult(4.1,5)
 print(z)
